09d7cba8c8b32786c8a9d2bce2f1922e388806db
Saud Amir <ho-htd-atif@unionfabric.com>
2026-08-05 18:23:13 +0500

    update every changes

--- lib/ads.ts
+++ lib/ads.ts
@@ -20,14 +20,14 @@ export const ADS = {
 <div id="container-ccc919cfe979b5e9a28f025954dde80d"></div>`,
   left: `<script>
   atOptions = {
-    'key' : '5a618e75cf4f19185a0ec1ba6a2ddb43',
+    'key' : '6ad4fc1b924b4e28f83e7319a1d12974',
     'format' : 'iframe',
-    'height' : 50,
-    'width' : 320,
+    'height' : 40,
+    'width' : 728,
     'params' : {}
   };
 </script>
-<script src="https://www.highperformanceformat.com/5a618e75cf4f19185a0ec1ba6a2ddb43/invoke.js"></script>
+<script src="https://www.highperformanceformat.com/6ad4fc1b924b4e28f83e7319a1d12974/invoke.js"></script>
 `,
   right: `<script async="async" data-cfasync="false" src="https://pl30550652.effectivecpmnetwork.com/4b09122a00629a80aacda2204d2eedec/invoke.js"></script>
 <div id="container-4b09122a00629a80aacda2204d2eedec"></div>
@@ -56,14 +56,14 @@ export const ADS = {
 `,
 bottom: `<script>
   atOptions = {
-    'key' : 'b1046b92a7ef244d82323a5160f441fd',
+    'key' : 'b2b7735d5d92d47cb9bcbe176a0c1ea9',
     'format' : 'iframe',
-    'height' : 50,
-    'width' : 320,
+    'height' : 250,
+    'width' : 300,
     'params' : {}
   };
 </script>
-<script src="https://www.highperformanceformat.com/b1046b92a7ef244d82323a5160f441fd/invoke.js"></script>
+<script src="https://www.highperformanceformat.com/b2b7735d5d92d47cb9bcbe176a0c1ea9/invoke.js"></script>
 `,
   pop: `<script src="https://pl30593808.effectivecpmnetwork.com/48/c6/7e/48c67e6ae0eb4dfc9dae2bc65af5a5c5.js"></script>
 
